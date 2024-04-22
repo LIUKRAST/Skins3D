7de4cbdd14305103d44190d6846f7b4e4b9a9e59
LIUKRAST <darthlucavader@gmail.com>
2024-04-23 00:56:08 +0200

    Added page.png

---
+++ src/main/java/net/frozenblock/skins3d/model/ExampleClass.java
@@ -0,0 +1,10 @@
+package net.frozenblock.skins3d.model;
+
+import net.minecraft.entity.LivingEntity;
+
+public class ExampleClass {
+
+    public static void something(LivingEntity entity) {
+        entity.tick();
+    }
+}
