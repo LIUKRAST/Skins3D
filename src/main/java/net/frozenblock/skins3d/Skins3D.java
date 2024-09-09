09a0e533389eff17eafb84690de67b6473a56e75
LiukRast <darthlucavader@gmail.com>
2024-09-09 03:03:56 +0200

    Update 3.0.1 - Added built-in blink texture pack - Fixed issue #6

--- src/main/java/net/frozenblock/skins3d/Skins3D.java
+++ src/main/java/net/frozenblock/skins3d/Skins3D.java
@@ -1,24 +1,39 @@
 package net.frozenblock.skins3d;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import java.nio.file.Path;
 import java.util.Objects;
 
 public class Skins3D implements ModInitializer {
 
+    public static final String MOD_ID = "skins3d";
+
     public static boolean configRes1;
     public static boolean configRes2;
     public static int configRes3;
     public static final boolean configRes4 = false;
 
-    public static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("liukrast3dskins.json");
+    public static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("liukrast" + MOD_ID + ".json");
 
     @Override
     public void onInitialize() {
         Skins3D.configRes1 = Boolean.parseBoolean((String) Config.getConfig("player"));
         Skins3D.configRes2 = Boolean.parseBoolean((String) Config.getConfig("player.heads"));
         Skins3D.configRes3 = Integer.parseInt((String) Objects.requireNonNull(Config.getConfig("resolution")));
+
+        final ModContainer container = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow();
+        ResourceManagerHelper.registerBuiltinResourcePack(
+                Identifier.of(MOD_ID, "blink"),
+                container,
+                Text.literal("Blinking Default"),
+                ResourcePackActivationType.DEFAULT_ENABLED
+        );
     }
 }
--- src/main/java/net/frozenblock/skins3d/mixins/PlayerEntityModelMixin.java
+++ src/main/java/net/frozenblock/skins3d/mixins/PlayerEntityModelMixin.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
 import net.frozenblock.skins3d.Skins3D;
 import net.frozenblock.skins3d.model.CustomPlayerModel;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.model.Dilation;
 import net.minecraft.client.model.ModelData;
 import net.minecraft.client.model.ModelPart;
@@ -44,7 +45,11 @@ public class PlayerEntityModelMixin {
 
     @Inject(at = @At("TAIL"), method = "setAngles(Lnet/minecraft/entity/LivingEntity;FFFFF)V")
     private void render(LivingEntity livingEntity, float f, float g, float time, float i, float j, CallbackInfo ci) {
-        face.hidden = Math.cos(time/5f + livingEntity.getUuid().variant()) < 0.95f;
+        if(MinecraftClient.getInstance().player == livingEntity && MinecraftClient.getInstance().options.getPerspective().isFirstPerson()) {
+            face.hidden = true;
+        } else {
+            face.hidden = Math.cos(time / 5f + livingEntity.getUuid().variant()) < 0.95f;
+        }
         if(Skins3D.configRes4) {
             face_root.copyTransform(root.getChild("head"));
             face.yaw = (float) Math.PI;
